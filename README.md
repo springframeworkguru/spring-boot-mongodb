627c8f85c0bfc1aada9004809647fb9bdc3082a5
John Thompson <john@springframework.guru>
2017-01-10 13:10:08 -0500

    initial project commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring-boot-mongodb
+This repository contains a Spring Boot example project for MongoDB.
+
+You can learn more about my courses [here](http://courses.springframework.guru/courses/) on my site.
---
+++ src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbApplication.class, args);
+	}
+}
