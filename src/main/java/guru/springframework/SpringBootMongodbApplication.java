126264f89c026c15e8bfa4a3bd78b5c853fa7469
Ximanta <ximanta.sarma@gmail.com>
2017-07-31 12:56:23 +0530

    Used embedded MongoDB

--- src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -1,12 +1,21 @@
 package guru.springframework;
 
+import com.mongodb.MongoClient;
+import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class SpringBootMongodbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
 	}
+
 }
---
+++ src/main/java/guru/springframework/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.config;
+
+import java.io.IOException;
+import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.*;
+import com.mongodb.MongoClient;
+
+
+@Configuration
+public class MongoConfig {
+
+    private static final String MONGO_DB_URL = "localhost";
+    private static final String MONGO_DB_NAME = "embeded_db";
+    @Bean
+    public MongoTemplate mongoTemplate() throws IOException {
+        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+        mongo.setBindIp(MONGO_DB_URL);
+        MongoClient mongoClient = mongo.getObject();
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+        return mongoTemplate;
+    }
+}
